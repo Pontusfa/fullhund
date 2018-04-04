72d77906cdfe977f5c8201dfd1c32d9f417572fb
Pontus Falk <pontus.falk@lightsinline.com>
2018-04-04 19:59:40 +0200

    java 10

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,12 +1,12 @@
 import lk.pontusfa.fullhund.App;
 import org.junit.Test;
 
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertNotNull;
 
 public class AppTest {
     @Test public void testAppHasAGreeting() {
-        App classUnderTest = new App();
-        assertNull("app should have a greeting", classUnderTest.getGreeting());
+        var classUnderTest = new App();
+        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
 
     }
 }
