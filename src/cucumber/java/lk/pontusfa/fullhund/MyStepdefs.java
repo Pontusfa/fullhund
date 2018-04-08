f4959ca348866456e6d87d3aab64219d56da20b5
Pontus Falk <pontus.falk@lightsinline.com>
2018-04-08 19:36:33 +0200

    jacoco

--- src/cucumber/java/lk/pontusfa/fullhund/MyStepdefs.java
+++ src/cucumber/java/lk/pontusfa/fullhund/MyStepdefs.java
@@ -3,6 +3,8 @@ package lk.pontusfa.fullhund;
 import cucumber.api.PendingException;
 import cucumber.api.java.en.When;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class MyStepdefs {
     @When("^I run a failing step$")
     public void iRunAFailingStep() {
@@ -12,5 +14,14 @@ public class MyStepdefs {
 
     @When("^I run a failing step1$")
     public void iRunAFailingSte1() {
+        App classUnderTest = new App();
+        assertNotNull(classUnderTest.getGreeting());
+        System.out.println(classUnderTest.getGreeting());
+    }
+
+    @When("^apa$")
+    public void apa() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
     }
 }
