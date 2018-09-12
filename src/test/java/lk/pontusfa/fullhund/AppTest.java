129008dbe560221b8bf6d934cfd430504bab311a
Pontus Falk <github@pontusfa.lk>
2018-09-12 21:09:22 +0200

    jacoco

--- src/test/java/lk/pontusfa/fullhund/AppTest.java
+++ src/test/java/lk/pontusfa/fullhund/AppTest.java
@@ -8,23 +8,16 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 class AppTest {
     @Test
     void testAppHasAGreeting1() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting1(true));
-        assertNull(classUnderTest.getGreeting1(false));
+
     }
 
     @Test
     void testAppHasAGreeting3() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting3(true));
-        assertNull(classUnderTest.getGreeting3(false));
+
     }
 
     @Test
     void testAppHasAGreeting4() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting4(true));
-        assertNull(classUnderTest.getGreeting4(false));
-        App.main(new String[]{""});
+
     }
 }
